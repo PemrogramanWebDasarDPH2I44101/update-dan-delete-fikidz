6945fd0904c57bee15479dde2c651d6188f953ff
Fiki Dzulfiqar <33850678+fikidz@users.noreply.github.com>
2018-10-05 21:17:15 +0700

    Update form.php

--- form.php
+++ form.php
@@ -1,13 +1,26 @@
-<!DOCTYPE html>
-    <head>
-        <title></title>
-    </head>
-    <body>
-        <form action="submit.php" method="post">
-            Nama<input type="text" name="nama" id=""><br>
-            Nim<input type="text" name="nim" id=""><br>
-            Tanggal Lahir<input type="date" name="tgl_lahir" id=""><br>
-            <input type="submit" value="Kirim">
-        </form>
-    </body>
+<html>
+<body>
+    <form action="index.php" method="post">
+    <table align="center">
+    	<tr>
+    		<td colspan="2" align="center"><h2>Data Diri</h2></td>
+    	</tr>
+    	<tr>
+    		<td>NIM</td>
+    		<td><input type="text" name="nim"></td>
+    	</tr>
+    	<tr>
+    		<td>Nama</td>
+    		<td><input type="text" name="nama"></td>
+    	</tr>
+    	<tr>
+    		<td>Tanggal Lahir</td>
+    		<td><input type="date" name="tgl_lahir"></td>
+    	</tr>
+    	<tr>
+    		<td colspan="2"><input type="submit" name="submit" value="Submit"></td>
+    	</tr>
+    </table>
+    </form>
+</body>
 </html>
